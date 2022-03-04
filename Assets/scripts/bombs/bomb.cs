de5707571568b62b8705b19475cf95a9608f3801
Vicente Viera <plantedcoot706@gmail.com>
2022-03-03 21:25:40 -0600

    First Due

---
+++ Assets/scripts/bombs/bomb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    Vector3 sInicial = new Vector3(0,0,0);
+    [Tooltip("Escala de la explosion")]
+    public Vector3 sFinal = new Vector3(10f,10f,10f);
+    float tTotal;
+    [Tooltip("Tiempo que tarda en alcanzar su maximo tamaño la explosion")]
+    public float tFinal = 5f;
+    float porcentaje = 0;
+    GameObject explosion;
+    bool contact = false;
+
+    void OnCollisionEnter(Collision col){
+        if(!contact && col.gameObject.tag != "Bomb"){
+            contact = true;
+
+            explosion = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            explosion.transform.position = this.transform.position;
+            explosion.tag="Bomb";
+        }
+    }
+
+    void Update(){
+        if(contact && porcentaje <= 1f ){
+            tTotal += Time.deltaTime;
+            porcentaje = tTotal/tFinal;
+            explosion.transform.localScale = Vector3.Lerp(sInicial,sFinal, porcentaje);
+        }
+    }
+}
--- Assets/scripts/plane/planeFlight.cs
+++ Assets/scripts/plane/planeFlight.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class planeFlight : MonoBehaviour
 {
+    //Cams
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
+    public GameObject cam5;
+    int camN;
     //forward speed
     [Tooltip("Velocidad de avance inicial")]
     public float speed;
@@ -11,12 +18,17 @@ public class planeFlight : MonoBehaviour
     [Tooltip("Velocidad de giro horizontal")]
     public float rSpeed;
     [Tooltip("Rotacion vertical")]
+    //Rotational movement
     public float rotation;
-
     [Tooltip("Rotacion horizontal")]
     public float hRotation;
+    //GUI para mostrar
     [Tooltip("GUI Seleccionada")]
     public GameObject GUI;
+    //Obeto de bomba
+    [Tooltip("Objeto que va a soltar como Bomba")]
+    public Rigidbody Bomb;
+    public Transform offSet;
 
 
     // Start is called before the first frame update
@@ -25,6 +37,11 @@ public class planeFlight : MonoBehaviour
         rSpeed=0;
         rotation=0;
         hRotation=0;
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+        cam3.SetActive(false);
+        cam4.SetActive(false);
+        cam5.SetActive(false);
     }
 
     // Update is called once per frame
@@ -61,10 +78,57 @@ public class planeFlight : MonoBehaviour
         Vector3 a = new Vector3(rotation*5f,0,hRotation*5f);
 
         transform.Rotate(a, Space.Self);
+
+        //drop bomb
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Rigidbody instance;
+            instance = Instantiate(Bomb, offSet.position, offSet.rotation) as Rigidbody;
+        }
+
+        //change camera
+        if (Input.GetKeyDown("c")){
+            camN++;
+            switch(camN){
+                case 1:
+                    cam1.SetActive(false);
+                    cam2.SetActive(true);
+                    cam3.SetActive(false);
+                    cam4.SetActive(false);
+                    cam5.SetActive(false);
+                    break;
+                case 2:
+                    cam1.SetActive(false);
+                    cam2.SetActive(false);
+                    cam3.SetActive(true);
+                    cam4.SetActive(false);
+                    cam5.SetActive(false);
+                    break;
+                case 3:
+                    cam1.SetActive(false);
+                    cam2.SetActive(false);
+                    cam3.SetActive(false);
+                    cam4.SetActive(true);
+                    cam5.SetActive(false);
+                    break;
+                case 4:
+                    cam1.SetActive(false);
+                    cam2.SetActive(false);
+                    cam3.SetActive(false);
+                    cam4.SetActive(false);
+                    cam5.SetActive(true);
+                    break;
+                case 5:
+                    cam1.SetActive(true);
+                    cam2.SetActive(false);
+                    cam3.SetActive(false);
+                    cam4.SetActive(false);
+                    cam5.SetActive(false);
+                    camN=0;
+                    break;
+            }
+        }
     }
 
-
-    
     void OnCollisionEnter(Collision col) {
         GUI.SetActive(true);
     }
